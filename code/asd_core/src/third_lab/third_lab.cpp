610ba93453531ebfc958d3009bbf9fe4a03613e6
DimitriiTrater <dima1603040@gmail.com>
2023-10-10 19:59:20 +0200

    good fast alg for third

--- code/asd_core/src/third_lab/third_lab.cpp
+++ code/asd_core/src/third_lab/third_lab.cpp
@@ -7,28 +7,48 @@
 #include <sys/types.h>
 #include <vector>
 
-void simple(long long int x) {
-  for (int i = 3; i < x + 1; i++) {
-    auto temp = i;
-    int k{0}, l{0}, m{0};
-    while (temp % 3 == 0) {
-      k++;
-      temp /= 3;
-    }
+// this is a very slow algorithm
+// void simple(long long int x) {
+//   for (int i = 3; i < x + 1; i++) {
+//     auto temp = i;
+//     int k{0}, l{0}, m{0};
+//     while (temp % 3 == 0) {
+//       k++;
+//       temp /= 3;
+//     }
+//
+//     while (temp % 5 == 0) {
+//       l++;
+//       temp /= 5;
+//     }
+//
+//     while (temp % 7 == 0) {
+//       m++;
+//       temp /= 7;
+//     }
+//
+//     if (temp == 1)
+//       std::cout << i << " =  3^" << k << " *  5^" << l << " * 7^" << m
+//                 << std::endl;
+//   }
+// }
 
-    while (temp % 5 == 0) {
-      l++;
-      temp /= 5;
+void simple(long long x) {
+  std::vector<long long int> res{};
+  for (long long int m{0}; std::pow(7, m) <= x; m++) {
+    for (long long int l{0}; std::pow(5, l) <= x; l++) {
+      for (long long int k{0}; std::pow(3, k) <= x; k++) {
+        long long int tmp = std::pow(3, k) * std::pow(5, l) * std::pow(7, m);
+        if (tmp <= x)
+          res.push_back(tmp);
+        else
+          break;
+      }
     }
-
-    while (temp % 7 == 0) {
-      m++;
-      temp /= 7;
-    }
-
-    if (temp == 1)
-      std::cout << i << " =  3^" << k << " *  5^" << l << " * 7^" << m
-                << std::endl;
+  }
+  std::sort(res.begin(), res.end());
+  for (const auto &el : res) {
+    std::cout << el << std::endl;
   }
 }
 
