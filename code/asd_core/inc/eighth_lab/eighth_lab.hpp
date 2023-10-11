5f8abc063e6242827425eb03dc4a6d6f21a1c02c
DimitriiTrater <dima1603040@gmail.com>
2023-10-11 07:27:06 +0200

    radix for neg

--- code/asd_core/inc/eighth_lab/eighth_lab.hpp
+++ code/asd_core/inc/eighth_lab/eighth_lab.hpp
@@ -1,29 +1,43 @@
 #pragma once
 #include <algorithm>
+#include <array>
 #include <iostream>
 #include <vector>
 
 template <class Cont> void radix_sort(Cont &cont) {
   auto max = *std::max_element(cont.begin(), cont.end());
-  std::cout << max << std::endl;
+  std::vector<int> neg_res{};
+
+  for (auto &el : cont) {
+    if (el < 0) {
+      neg_res.push_back(el);
+      el = 0;
+    }
+  }
+
+  int pos_res[cont.size()] = {};
+
   for (int shift = 1; max / shift > 0; shift *= 10) {
 
-    int res[cont.size()];
-    int count[10] = {0};
+    std::array<int, 10> count{};
 
     for (int i = 0; i < cont.size(); i++)
-      count[(cont[i] / shift) % 10]++;
-
+      count[(static_cast<uint>(cont[i]) / shift) % 10]++;
     for (int i = 1; i < 10; i++)
       count[i] += count[i - 1];
-
     for (int i = cont.size() - 1; i >= 0; i--) {
-      res[count[(cont[i] / shift) % 10] - 1] = cont[i];
+      pos_res[count[(cont[i] / shift) % 10] - 1] = cont[i];
       count[(cont[i] / shift) % 10]--;
     }
+
     for (int i = 0; i < cont.size(); i++)
-      cont[i] = res[i];
+      cont[i] = pos_res[i];
   }
+
+  std::sort(neg_res.begin(), neg_res.end());
+
+  for (int i = 0; i < neg_res.size(); i++)
+    cont[i] = neg_res[i];
 }
 
 void eighth_start();
--- code/asd_core/inc/lnch_opts/lnch_opts.hpp
+++ code/asd_core/inc/lnch_opts/lnch_opts.hpp
@@ -2,8 +2,16 @@
 
 #include <iostream>
 #include <optional>
-
+namespace utils {
+template <class Cont> void print(Cont &cont) {
+  for (auto v : cont) {
+    std::cout << v << " ";
+  }
+  std::cout << "\n";
+}
+} // namespace utils
 namespace lnch_opts {
+
 void build_hello();
 void choice_lab(int inp);
 } // namespace lnch_opts
